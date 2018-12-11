b2392f44cb1bcd5b7269b1ea2a04a9a77f9e44ea
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-12-11 12:16:48 -0500

    got the main app running

--- cf_project/settings.py
+++ cf_project/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_extensions'
+    'django_extensions',
+    'main'
 ]
 
 MIDDLEWARE = [
