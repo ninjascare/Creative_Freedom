168cea076e1b270d9ea466b1940af1ae248e0abe
Shivank Rao <shivankrao@Shivanks-MacBook-Pro.local>
2018-12-11 16:08:27 -0500

    serializers

--- main/views.py
+++ main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .serializers import UserSerializer, PostSerializer, CommentSerializer
+from .models import User, Post, Comment
+
+
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class PostView(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CommentView(viewsets.CommentViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
