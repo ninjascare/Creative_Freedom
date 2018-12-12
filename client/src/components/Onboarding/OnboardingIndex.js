53839648d733b8931bef70d1745af874d997e837
CB_ATL_110_David_Williams <cb_atl_110_david_williams@MacBook-Air-87.local>
2018-12-12 12:39:57 -0500

    Css file now connected to the onboarding1 component. Ready 4 mass style

--- client/src/components/Onboarding/OnboardingIndex.js
+++ client/src/components/Onboarding/OnboardingIndex.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import '../../styles.css';
+import Onboarding1 from '../Onboarding/Onboarding1'
 
 class OnboardingPage extends Component {
     render() {
         return (
             <div>
                 <h1>Hello from the Onboarding Index page of swagginess</h1>
+
+                <Onboarding1/>
+                
+                
             </div>
         );
     }
