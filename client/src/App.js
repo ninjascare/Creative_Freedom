e5cc48908e41a84bf710429424d01b43e16b7178
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-12-12 15:13:04 -0500

    ghfsdc

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import './App.css';
-import './styles.css';
-import OnboardingPage from './components/Onboarding/OnboardingIndex';
+import "./App.css";
+import "./styles.css";
+import OnboardingPage from "./components/Onboarding/OnboardingIndex";
 
 class App extends Component {
   render() {
     return (
       <div>
-        
         {/* This handles all of our routing stuff. */}
         <Router>
-
-        <div>
-
-          <Switch>
-
-            <Route exact path="/" component={OnboardingPage} />
-
-          </Switch>
-
-        </div>
-
+          <div>
+            <Switch>
+              <Route exact path="/" component={OnboardingPage} />
+            </Switch>
+          </div>
         </Router>
-
       </div>
     );
   }
--- client/src/components/Onboarding/OnboardingIndex.js
+++ client/src/components/Onboarding/OnboardingIndex.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
-import '../../styles.css';
-import Onboarding1 from '../Onboarding/Onboarding1'
-import Onboarding2 from '../Onboarding/Onboarding2'
-import Onboarding3 from '../Onboarding/Onboarding3'
-import Onboarding4 from '../Onboarding/Onboarding4'
-
+import React, { Component } from "react";
+import "../../styles.css";
+import Onboarding1 from "../Onboarding/Onboarding1";
+import Onboarding2 from "../Onboarding/Onboarding2";
+import Onboarding3 from "../Onboarding/Onboarding3";
+import Onboarding4 from "../Onboarding/Onboarding4";
 
 class OnboardingPage extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello from the Onboarding Index page of swagginess</h1>
-
-                <Onboarding1/>
-                <Onboarding2/>
-                <Onboarding3/>
-                <Onboarding4/>
-
-                
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Hello from the Onboarding Index page of swagginess</h1>
+        <Onboarding1 />
+        <Onboarding2 />
+        <Onboarding3 />
+        <Onboarding4 />
+      </div>
+    );
+  }
 }
 
 export default OnboardingPage;
