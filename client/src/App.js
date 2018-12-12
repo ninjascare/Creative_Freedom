24bde19972208803b9924feeee362d568d654be4
CB_ATL_110_David_Williams <cb_atl_110_david_williams@MacBook-Air-87.local>
2018-12-12 11:59:37 -0500

    Onboarding page name change to index. Last commit before branch creation

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
-import OnboardingPage from '../src/components/Onboarding/OnboardingPage';
+import OnboardingPage from './components/Onboarding/OnboardingIndex';
 
 class App extends Component {
   render() {
