f313381b577842621b5847c235f2fd5d907a5619
Rguy <rguy1290@gmail.com>
2018-12-12 13:37:59 -0500

    added more boilerplates

--- client/src/components/Onboarding/OnboardingIndex.js
+++ client/src/components/Onboarding/OnboardingIndex.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import '../../styles.css';
 import Onboarding1 from '../Onboarding/Onboarding1'
+import Onboarding2 from '../Onboarding/Onboarding2'
+import Onboarding3 from '../Onboarding/Onboarding3'
+import Onboarding4 from '../Onboarding/Onboarding4'
+
 
 class OnboardingPage extends Component {
     render() {
@@ -9,7 +13,10 @@ class OnboardingPage extends Component {
                 <h1>Hello from the Onboarding Index page of swagginess</h1>
 
                 <Onboarding1/>
-                
+                <Onboarding2/>
+                <Onboarding3/>
+                <Onboarding4/>
+
                 
             </div>
         );
