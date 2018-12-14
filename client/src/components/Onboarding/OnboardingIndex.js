21af69f7610f2156cc9e14a281c228dd9aae26ec
CB_ATL_110_David_Williams <cb_atl_110_david_williams@MacBook-Air-87.local>
2018-12-14 13:20:09 -0500

    killed the code ghost! css complete!

--- client/src/components/Onboarding/OnboardingIndex.js
+++ client/src/components/Onboarding/OnboardingIndex.js
@@ -6,60 +6,60 @@ import Onboarding2 from "../Onboarding/Onboarding2";
 import Onboarding3 from "../Onboarding/Onboarding3";
 import Onboarding4 from "../Onboarding/Onboarding4";
 
-// const BrightDots = styled.div`
-// a:hover {
-//     background-color: white;
-// }
-// `
+const BrightDots = styled.div`
+a:active {
+    background-color: white;
+}
+a:hover {
+    background-color: white;
+}
+`
+
 
 class OnboardingPage extends Component {
     render() {
         return (
             <div>
-                    {/* ↓ next task is getting these circles to act as link buttons for the scroll functionality to work! ↓ */}
+                {/* ↓ next task is getting these circles to act as link buttons for the scroll functionality to work! ↓ */}
 
-                
+
                 <div className='onboardingSlideContainer'>
 
-                <div className='dotContainer'>
+                    <div className='dotContainer'>
 
-                {/* <BrightDots> */}
-                <a href="#board1"> 
-                <div className='dotSelectors' id='one'></div>
-                </a>
+                        <BrightDots>
+                            <a href="#board1" className='dotSelectors' id='one'>
+                            </a>
 
-                <a href="#board2">
-                <div className='dotSelectors' id='two' ></div>
-                </a>
+                            <a href="#board2" className='dotSelectors' id='two'>
+                            </a>
 
-                <a href="#board3">
-                <div className='dotSelectors' id='three'></div>
-                </a>
+                            <a href="#board3" className='dotSelectors' id='three'>
+                            </a>
 
-                <a href="#board4">
-                <div className='dotSelectors' id='four' ></div> 
-                </a>
-                {/* </BrightDots> */}
+                            <a href="#board4" className='dotSelectors' id='four'>
+                            </a>
+                        </BrightDots>
 
-                </div>
+                    </div>
 
-                <div className='onboarding1' id='board1'>
-                <Onboarding1/>
-                </div>
+                    <div className='onboarding1' id='board1'>
+                        <Onboarding1 />
+                    </div>
 
 
-                <div className='onboarding2' id='board2'>
-                <Onboarding2/>
-                </div>
+                    <div className='onboarding2' id='board2'>
+                        <Onboarding2 />
+                    </div>
 
-                <div className='onboarding3' id='board3'>
-                <Onboarding3/>
-                </div>
+                    <div className='onboarding3' id='board3'>
+                        <Onboarding3 />
+                    </div>
 
-                <div className='onboarding4' id='board4'>
-                <Onboarding4/>
-                </div>
-                
+                    <div className='onboarding4' id='board4'>
+                        <Onboarding4 />
+                    </div>
+
                 </div>
 
             </div>
