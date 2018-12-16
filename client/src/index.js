66eb6e155e0123428d31aacb5b2f280fa3aeda0c
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-12-16 12:37:23 -0500

    stil working on forms

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './test.css';
 import App from './App';
 // import 'font-awesome/css/font-awesome.min.css'; 
-import '@fortawesome/fontawesome-free/css/all.min.css';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
