aa4e46ea0b290100d0171f69c7361457d8deb706
Rguy <rguy1290@gmail.com>
2018-12-17 16:19:51 -0500

    added media queries for phone size and full screen, still need to get one for everything in between

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const Nav = styled.div`
   background-color: black;
   background-position: fixed;
-  margin-top: -1%;
+  margin-top: -2%;
   a:link {
     text-decoration: none;
   }
