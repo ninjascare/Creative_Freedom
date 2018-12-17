addf94e685c2df002dd36feec3ae6b2e2e2a639d
CB_ATL_110_David_Williams <cb_atl_110_david_williams@MacBook-Air-87.local>
2018-12-17 02:08:53 -0500

    Got the post page looking uniform. Profile pic/info next.

--- client/src/components/Post/PostIndex.js
+++ client/src/components/Post/PostIndex.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import styled from 'styled-components'
+
+
 
 class PostIndex extends Component {
   state = {
@@ -22,20 +25,34 @@ class PostIndex extends Component {
   render() {
     return (
       <div>
+
         <h1>Post Index</h1>
         <h2>News Feed</h2>
+
         {this.state.posts.map(post => (
           <div key={post._id}>
+
+          <div className='postStyle'>
+
+            <div className='Profile'>
             <Link to={"/member/:userId"}>
-              {post.user.name} <br />
+              <img src={post.user.image_url} alt="" className='postProfileImg' />
+              
+              <h1>{post.user.name}</h1>
+              <h5>{post.user.title}</h5> 
             </Link>{" "}
+            </div>
+
             <br />
             {post.content} <br />
             {post.created_at} <br />
+
             {/* trying to make audio work with different methods */}
             {post.audio_url ? (
               <audio controls src={post.audio_url} type="audio/mpeg" />
             ) : null}
+
+            
             <br />
             <img src={post.image_url} />
             <iframe src={post.video_url} frameborder="100" /> <br />
@@ -48,6 +65,8 @@ class PostIndex extends Component {
                 </div>
               ))}
             </button>
+            </div>
+
           </div>
         ))}
 
