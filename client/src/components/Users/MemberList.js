440fd03c32e53314d22eb8dd191c176b6b30ca8c
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-12-18 18:52:30 -0500

    yeah boi

--- client/src/components/Users/MemberList.js
+++ client/src/components/Users/MemberList.js
@@ -25,7 +25,7 @@ export default class MemberList extends Component {
         </h1>
 
         <div className='dummySearchBar'>
-          <div>Search</div> 🔍</div>
+          <div>Search</div> 🔍 </div>
 
         {this.state.users.map(user => (
           <div key={user._id} className='userContainer'>
@@ -33,8 +33,6 @@ export default class MemberList extends Component {
             <Link to={`/member/${user.id}`} className='peskyAtag'>
 
               <img src={user.image_url} alt="dfg" className='memberPic' />
-
-
               <div className='userInfo1'>
                 <h2>{user.name}</h2>
                 <h2>{user.title}</h2>
