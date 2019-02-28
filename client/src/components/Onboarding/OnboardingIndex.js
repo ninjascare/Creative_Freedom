baa8941f65a9a1a53ff0ba8c7cebdf22e6ed7cd2
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2019-02-28 18:34:38 -0500

    Adapt build scripts to Heroku changes

--- client/src/components/Onboarding/OnboardingIndex.js
+++ client/src/components/Onboarding/OnboardingIndex.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 import "../../styles.css";
 import Onboarding1 from "../Onboarding/Onboarding1";
 import Onboarding2 from "../Onboarding/Onboarding2";
@@ -7,64 +7,52 @@ import Onboarding3 from "../Onboarding/Onboarding3";
 import Onboarding4 from "../Onboarding/Onboarding4";
 
 const BrightDots = styled.div`
-a:active {
+  a:active {
     background-color: white;
-}
-a:hover {
+  }
+  a:hover {
     background-color: white;
-}
-`
-
+  }
+`;
 
 class OnboardingPage extends Component {
-    render() {
-        return (
-            <div>
-                {/* ↓ next task is getting these circles to act as link buttons for the scroll functionality to work! ↓ */}
+  render() {
+    return (
+      <div>
+        {/* ↓ next task is getting these circles to act as link buttons for the scroll functionality to work! ↓ */}
 
+        <div className="onboardingSlideContainer">
+          <div className="dotContainer">
+            <BrightDots>
+              <a href="#board1" className="dotSelectors" id="one" />
 
-                <div className='onboardingSlideContainer'>
+              <a href="#board2" className="dotSelectors" id="two" />
 
-                    <div className='dotContainer'>
+              <a href="#board3" className="dotSelectors" id="three" />
 
-                        <BrightDots>
-                            <a href="#board1" className='dotSelectors' id='one'>
-                            </a>
+              <a href="#board4" className="dotSelectors" id="four" />
+            </BrightDots>
+          </div>
 
-                            <a href="#board2" className='dotSelectors' id='two'>
-                            </a>
+          <div className="onboarding1" id="board1">
+            <Onboarding1 />
+          </div>
 
-                            <a href="#board3" className='dotSelectors' id='three'>
-                            </a>
+          <div className="onboarding2" id="board2">
+            <Onboarding2 />
+          </div>
 
-                            <a href="#board4" className='dotSelectors' id='four'>
-                            </a>
-                        </BrightDots>
+          <div className="onboarding3" id="board3">
+            <Onboarding3 />
+          </div>
 
-                    </div>
-
-                    <div className='onboarding1' id='board1'>
-                        <Onboarding1 />
-                    </div>
-
-
-                    <div className='onboarding2' id='board2'>
-                        <Onboarding2 />
-                    </div>
-
-                    <div className='onboarding3' id='board3'>
-                        <Onboarding3 />
-                    </div>
-
-                    <div className='onboarding4' id='board4'>
-                        <Onboarding4 />
-                    </div>
-
-                </div>
-
-            </div>
-        );
-    }
+          <div className="onboarding4" id="board4">
+            <Onboarding4 />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default OnboardingPage;
